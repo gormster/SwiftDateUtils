0e014770d2189dc48512436cac42dad7b6e32b60
Morgan <gormster@me.com>
2014-06-06 01:40:26 +1000

    Initial Commit

---
+++ SwiftDateUtils/SwiftDateUtils.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDateUtils.h
+//  SwiftDateUtils
+//
+//  Created by Morgan Harris on 6/06/2014.
+//  Copyright (c) 2014 Morgan Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDateUtils.
+FOUNDATION_EXPORT double SwiftDateUtilsVersionNumber;
+
+//! Project version string for SwiftDateUtils.
+FOUNDATION_EXPORT const unsigned char SwiftDateUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDateUtils/PublicHeader.h>
+
+
